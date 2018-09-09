2ba110d778dd4fc86d12bf748d988ee726f10647
Maxim Tishenko <maxim.tis96@gmail.com>
2018-09-10 02:35:17 +0300

    add metrics factory

---
+++ FlashTuna.Core/Common/Metric/MetricsFactory/MetricsFactory.cs
@@ -0,0 +1,19 @@
+﻿using FlashTuna.Core.Common.Metric.Interfaces;
+using FlashTuna.Core.Common.PerfomanceMetrics;
+using FlashTuna.Core.Common.PerfomanceMetrics.OperitionMetric;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlashTuna.Core.Common.Metric.MetricsFactory
+{
+    public class MetricsFactory
+    {
+        public static IMetric CreateMetric(string methodName,
+                              string tag,
+                              string moduleName)
+        {
+            return new OperationMetric(methodName, tag, moduleName);
+        }
+    }
+}
