1872591b8f2ea35586fe7df73f008586a75d8abc
Maxim Tishenko <maxim.tis96@gmail.com>
2018-10-03 02:35:18 +0300

    enable attributes

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -12,14 +12,51 @@ namespace DemoApp
 
     class ProductionClassA : MeteredClass
     {
+        public ProductionClassA() : base(typeof(ProductionClassA))
+        {
+
+        }
         [OperationMetric(nameof(ProductionClassA))]
-        void LongOperation()
+        public void LongOperation()
         {
             int i = 10;
             while (i > 0)
             {
                 Console.WriteLine("Do Iteration!");
                 Thread.Sleep(1000);
+                i--;
+            }
+        }
+    }
+
+    class ProductionClassB : MeteredClass
+    {
+        public ProductionClassB() : base(typeof(ProductionClassA))
+        {
+
+        }
+
+        [OperationMetric(nameof(ProductionClassB))]
+        public void LongOperation()
+        {
+            int i = 10;
+            while (i > 0)
+            {
+                Console.WriteLine("Do Iteration!");
+                Thread.Sleep(1000);
+                i--;
+            }
+        }
+
+        [OperationMetric(nameof(ProductionClassB))]
+        public void ShortOperation()
+        {
+            int i = 10;
+            StartRecording();
+            while (i > 0)
+            {
+                Console.WriteLine("Do Iteration!");
+                Thread.Sleep(100);
                 i--;
             }
         }
@@ -29,28 +66,26 @@ namespace DemoApp
 
     class Program
     {
-        private static void LongOperation()
-        {
-            int i = 10;
-            while (i > 0)
-            {
-                Console.WriteLine("Do Iteration!");
-                Thread.Sleep(1000);
-                i--;
-            }
-        }
+        
         public Program()
         {
             var _perfomanceMethods = this.GetType().GetMethods().Where(x => x.GetCustomAttributes(typeof(TaskMetricAttribute), true).Any());
         }
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Demo App started...");
-            LongOperation();
-            Console.WriteLine("Demo App finished...");
-            Console.ReadKey();
 
+            FlashTuna.Core.Configuration.FlashTuna.Initialize(
+                            new FlashTuna.Core.Configuration.FlashTuna.FlashTunaBuilder()
+                                                            .SetTimeLine(new TimeLine(null))
+                                                            .SetModuleName("Test Module")
+                                                            .Build());
+
+            ProductionClassA classA = new ProductionClassA();
+            classA.LongOperation();
+
+            ProductionClassB classB = new ProductionClassB();
+            classB.ShortOperation();
+            classB.LongOperation();
         }
     }
 }
--- FlashTuna.Core/Common/Metric/MeteredClass.cs
+++ FlashTuna.Core/Common/Metric/MeteredClass.cs
@@ -1,4 +1,5 @@
-﻿using FlashTuna.Core.Attributes.Common;
+﻿using FlashTuna.Core.Attributes;
+using FlashTuna.Core.Attributes.Common;
 using FlashTuna.Core.Common.Metric.Interfaces;
 using FlashTuna.Core.TimeLine;
 using System;
@@ -14,20 +15,36 @@ namespace FlashTuna.Core.Common.Metric
     public abstract class MeteredClass
     {
         private ITimeLine _timeLine;
-        private string _derivedClassName;
+        private Type _derivedClassName;
 
-        public MeteredClass([CallerMemberName] string className = null)
+        static MeteredClass()
         {
-            _derivedClassName = className;
+            var ass = Assembly.GetExecutingAssembly();//Use .GetCallingAssembly() if this method is in a library, or even both
+               var types = ass.GetTypes();
+            var methods = types.SelectMany(t => t.GetMethods());
+                   var hasAttr = methods.Where(m => m.GetCustomAttributes(typeof(OperationMetricAttribute), true).Length > 0)
+                      .ToList();
+        }
+        public MeteredClass(Type derivedClass)
+        {
+            _timeLine = FlashTuna.Core.Configuration.FlashTuna.CurrentTimeLine;
+            _derivedClassName = derivedClass;
+            Assembly clientAssembly = derivedClass.Assembly;
+
+            var ass = clientAssembly; //Assembly.GetExecutingAssembly();//Use .GetCallingAssembly() if this method is in a library, or even both
+            var types = ass.GetTypes();
+            var methods = types.SelectMany(t => t.GetMethods());
+            var hasAttr = methods.Where(m => m.GetCustomAttributes(typeof(OperationMetricAttribute), true).Length > 0)
+               .ToList();
         }
 
-        public void StartRecording([CallerMemberName] string methodName = null)
+        protected void StartRecording([CallerMemberName] string methodName = null)
         {
-            _timeLine.StartMetric(_derivedClassName, methodName);
+            _timeLine.StartMetric(_derivedClassName.Name, methodName);
         }
-        public void StopRecording([CallerMemberName] string methodName = null)
+        protected void StopRecording([CallerMemberName] string methodName = null)
         {
-            _timeLine.StopMetric(_derivedClassName, methodName);
+            _timeLine.StopMetric(_derivedClassName.Name, methodName);
         }
     }
 }
